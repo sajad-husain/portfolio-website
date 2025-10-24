aead6a40e2e5f0414f426345c8cb8708920500db
Sajjad Hussain <sajjadhusain8084@gmail.com>
2025-10-24 20:24:57 +0500

    Day 2 School paperwork problem

--- js-exercises/practiceJs.js
+++ js-exercises/practiceJs.js
@@ -7,7 +7,16 @@ function smash(words) {
     return words.join(" ")
 }
 
+// reverse string
 function reverString(myArray) {
     return myArray.reverse().join(" ")
+}
+
+// school paperwork problem 
+function paperWork(n, m) {
+    if (n <= 0 || m <= 0) {
+        return 0
+    }
+    return n * m
 }
 
