dfc0c4127f99849a947059ce61e7af3fd998081f
Sajjad Hussain <sajjadhusain8084@gmail.com>
2025-10-24 22:54:23 +0500

    Day 2 Test toggle class

--- script.js
+++ script.js
@@ -2,4 +2,6 @@ const toggleButton = document.getElementById("theme-toggle")
 
 toggleButton.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode")
+    console.log("toggle clicked");
+
 })
