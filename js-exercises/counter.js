f98c828ee34869ba79c305389e6e81e72e918ae9
Sajjad Hussain <sajjadhusain8084@gmail.com>
2025-10-27 13:48:52 +0500

    Day 4 Counter app completed

--- js-exercises/counter.js
+++ js-exercises/counter.js
@@ -1,4 +1,26 @@
 function setupCounter() {
+    let count = 0
 
+    const stepCounter = document.createElement("p")
+    stepCounter.textContent = `Counter: ${count}`
+    document.body.appendChild(stepCounter)
 
+    const incButton = document.createElement("button")
+    incButton.textContent = `Increment `
+    document.body.appendChild(incButton)
+
+    const decButton = document.createElement("button")
+    decButton.textContent = `Decrement`
+    document.body.appendChild(decButton)
+
+    incButton.addEventListener("click", () => {
+        count++
+        stepCounter.textContent = `Count: ${count}`
+    })
+
+    decButton.addEventListener("click", () => {
+        count--
+        stepCounter.textContent = `Count: ${count}`
+    })
 }
+setupCounter()
