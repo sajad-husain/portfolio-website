3246f6a35afacc2af79515b6b787e93d3c5382bd
Sajjad Hussain <sajjadhusain8084@gmail.com>
2026-05-16 23:25:24 +0500

    fixed app section error

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,4 +1,10 @@
-
+import { useState } from "react";
+import Hero from "./components/Hero";
+import About from "./components/About"
+import Skills from "./components/Skills"
+import Projects from "./components/Projects"
+import Contact from "./components/Contact"
+import Footer from "./components/Footer"
 function App() {
     // Dark mode state
     const [darkMode, setDarkMode] = useState(false);
