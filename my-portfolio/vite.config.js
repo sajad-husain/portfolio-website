632f536d514ff78a11166c9ebfa1a01ef3971ef8
Sajjad Hussain <sajjadhusain8084@gmail.com>
2026-05-17 00:05:29 +0500

    adjusting few files to publish this webpage

--- my-portfolio/vite.config.js
+++ my-portfolio/vite.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // ← Important: Root path ke liye
-  build: {
-    outDir: "dist", // ← Confirm: dist folder mein build hoga
-    emptyOutDir: true,
-  },
+  base: "/",
+  build: { outDir: "dist" },
 });
